82af678bdc06e8105c40847cc435693161a2f6e8
Lucas Sabino <112620334+Efledoscorre@users.noreply.github.com>
2024-08-22 19:28:46 -0300

    Add files via upload

---
+++ Atividade1/variaveisOperadores.js
@@ -0,0 +1,9 @@
+let n1 = 10; 
+let n2 = 8;
+
+console.log(`O resultado da soma entre ${n1} e ${n2} é ${n1 + n2}\n`);
+console.log(`O resultado da subtração entre ${n1} e ${n2} é ${n1 - n2}\n`);
+console.log(`O resultado da multiplicação entre ${n1} e ${n2} é ${n1 * n2}\n`);
+console.log(`O resultado da divisão entre ${n1} e ${n2} é ${n1 / n2}\n`);
+console.log(`O resultado do modulo entre ${n1} e ${n2} é ${n1 % n2}\n`);
+console.log(`O resultado da potência entre ${n1} e ${n2} é ${n1 ** n2}\n`);
